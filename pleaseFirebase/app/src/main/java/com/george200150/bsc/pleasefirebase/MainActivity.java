a5b0f4ccfc258064bb1b75b34df38d397d28eec9
george200150 <georgeciubotariu@yahoo.com>
2020-08-11 20:09:00 +0300

    solved good sized pictures transport over network

--- pleaseFirebase/app/src/main/java/com/george200150/bsc/pleasefirebase/MainActivity.java
+++ pleaseFirebase/app/src/main/java/com/george200150/bsc/pleasefirebase/MainActivity.java
@@ -1,14 +1,16 @@
 package com.george200150.bsc.pleasefirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,7 +27,11 @@ import com.george200150.bsc.pleasefirebase.service.APIService;
 import com.george200150.bsc.pleasefirebase.util.ApiUtils;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import retrofit2.Call;
@@ -41,17 +47,22 @@ public class MainActivity extends AppCompatActivity {
     private android.graphics.Bitmap photo;
     private Button button;
 
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private String currentPhotoPath;
     private APIService mAPIService;
 
-    private static Context mContext;
+    private static Context mContext; // TODO: CREATED MEMORY LEAK JUST FOR TESTING PURPOSES !!!
+
     public static Context getContext() {
         return mContext;
     }
+
     public static void setContext(Context context) {
         mContext = context;
     }
 
-    public static void doToast(String PLANT){
+    public static void doToast(String PLANT) { // TODO: CREATED MEMORY LEAK JUST FOR TESTING PURPOSES !!!
         mResponseTv2.setText(PLANT);
         Toast.makeText(MainActivity.getContext(), PLANT, Toast.LENGTH_LONG).show();
     }
@@ -83,8 +94,6 @@ public class MainActivity extends AppCompatActivity {
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //android.graphics.Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
-                //if (bitmap != null) {
                 if (photo != null) {
                     sendPost(photo);
                     imageView.setImageBitmap(photo);
@@ -97,29 +106,57 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.mssg_error_submitting_post, Toast.LENGTH_SHORT).show();
     }
 
-
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
     private void dispatchTakePictureIntent(View view) {
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
+    }
+
+    public android.graphics.Bitmap getResizedBitmap(android.graphics.Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return android.graphics.Bitmap.createScaledBitmap(image, width, height, true);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            android.graphics.Bitmap imageBitmap = (android.graphics.Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-            this.photo = imageBitmap;
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            android.graphics.Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, options);
+
+            photo = this.getResizedBitmap(bitmap, 500); // resize, big pictures are hard to be transported
+            imageView.setImageBitmap(photo);
         }
     }
 
-    public void sendPost(android.graphics.Bitmap androidBitmap) { // TODO: if camera is rotated other than landscape, w <-> h and python crashes
+    public void sendPost(android.graphics.Bitmap androidBitmap) {
         Bitmap bitmap = new Bitmap();
 
         int h = androidBitmap.getHeight();
@@ -130,12 +167,12 @@ public class MainActivity extends AppCompatActivity {
         bitmap.setHeight(h);
         bitmap.setWidth(w);
 
-        int[] androidPixels = new int[h*w];
+        int[] androidPixels = new int[h * w];
         androidBitmap.getPixels(androidPixels, 0, w, 0, 0, w, h);
 
         // build Pixel object from int pixel
         List<Pixel> pixels = new ArrayList<>();
-        for (int intPix : androidPixels){
+        for (int intPix : androidPixels) {
             int r = (intPix >> 16) & 0xff;
             int g = (intPix >> 8) & 0xff;
             int b = intPix & 0xff;
@@ -146,7 +183,6 @@ public class MainActivity extends AppCompatActivity {
             pixel.setB(b);
             pixels.add(pixel);
         }
-        //
 
         // TESTING... //
         /*int[] revertedPixels = new int[h*w];
@@ -167,115 +203,7 @@ public class MainActivity extends AppCompatActivity {
         revertedBitmap.setPixels(revertedPixels, 0, w, 0, 0, w, h);
         imageView.setImageBitmap(revertedBitmap);*/
 
-//        // inline stub for integration testing
-//        Bitmap bitmap = new Bitmap();
-//        /*bitmap.setHeight(9);
-//        bitmap.setWidth(9);*/
-//        bitmap.setHeight(3);
-//        bitmap.setWidth(3);
-//        List<Pixel> pixels = new ArrayList<>();
-//        Pixel white = new Pixel();
-//        white.setR(255);
-//        white.setG(255);
-//        white.setB(255);
-//
-//        Pixel black = new Pixel();
-//        black.setR(0);
-//        black.setG(0);
-//        black.setB(0);
-//
-//        pixels.add(white);
-//        pixels.add(black);
-//        pixels.add(white);
-//        pixels.add(black);
-//        pixels.add(white);
-//        pixels.add(black);
-//        pixels.add(white);
-//        pixels.add(black);
-//        pixels.add(white);
-
-        /*pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);
-        pixels.add(black);
-        pixels.add(white);*/
-
         bitmap.setPixels(pixels);
-        // change of plan - token will be generated by the client
 
         Token token = new Token(bitmap.hashCode() + "_TOKEN_" + System.nanoTime());
         ForwardMessage forwardMessage = new ForwardMessage(bitmap, token);
@@ -285,32 +213,45 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<Token> call, Response<Token> response) {
 
-                if(response.isSuccessful()) {
+                if (response.isSuccessful()) {
                     showResponse(response.body().toString());
                     Token token = response.body();
                     Log.i(TAG, "post submitted to API." + token.toString());
-
                 }
             }
 
             @Override
             public void onFailure(Call<Token> call, Throwable t) {
-
                 showErrorMessage();
                 Log.e(TAG, "Unable to submit post to API: {}", t);
             }
         });
     }
 
-    private void subscribeToTopic(String topic){
+    private void subscribeToTopic(String topic) {
         FirebaseMessaging.getInstance().subscribeToTopic(topic);
         Log.d(TAG, "subscribeToTopic: " + topic);
     }
 
     public void showResponse(String response) {
-        if(mResponseTv.getVisibility() == View.GONE) {
+        if (mResponseTv.getVisibility() == View.GONE) {
             mResponseTv.setVisibility(View.VISIBLE);
         }
         mResponseTv.setText(response);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
